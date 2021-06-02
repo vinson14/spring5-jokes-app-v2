8cf37f1d56ebfe91ec53998ecb7a161c98791957
Vinson Ong <vinson1493@gmail.com>
2021-06-02 12:42:55 +0800

    Completed first prototype

---
+++ src/main/java/guru/springframework/spring5jokesappv2/controllers/JokesController.java
@@ -0,0 +1,23 @@
+package guru.springframework.spring5jokesappv2.controllers;
+
+import guru.springframework.spring5jokesappv2.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokesController {
+
+    private final JokeService jokeService;
+
+    public JokesController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping(value={"", "/"})
+    public String getBooks(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+        return "index";
+    }
+
+}
---
+++ src/main/java/guru/springframework/spring5jokesappv2/services/ChuckNorrisJokeService.java
@@ -0,0 +1,15 @@
+package guru.springframework.spring5jokesappv2.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Primary
+@Service
+public class ChuckNorrisJokeService implements JokeService{
+    @Override
+    public String getJoke() {
+        ChuckNorrisQuotes chuckNorrisQuotes = new ChuckNorrisQuotes();
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
